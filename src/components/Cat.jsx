2e060787f8df2c13b4c4ae23a5c3acacc531d773
Wei <qwbu22@gmail.com>
2024-12-12 09:52:57 -0800

    state, petcount

---
+++ src/components/Cat.jsx
@@ -0,0 +1,34 @@
+// name: 'Ubik',
+// caretaker: 'Maria',
+// color: 'grey',
+// personality: 'wild child',
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+const Cat = ({ name, color, personality, caretaker }) => {
+    const [petCount,setPetCount] = useState(0)
+
+    const petClicked = () => {
+        setPetCount(petCount => petCount + 1);
+    }
+    return (
+            <li>
+                <h3>Name: {name}</h3>
+                <h3>Color: {color}</h3>
+                <h3>Caretaker: {caretaker}</h3>
+                <h3>Personality: {personality}</h3>
+                <p>$ of 🐈 : {petCount}</p>
+                <button onClick={petClicked}>Pet Cat</button>
+            </li>
+        );
+};
+
+Cat.propTypes = {
+    name: PropTypes.string.isRequired,
+    color:PropTypes.string.isRequired,
+    personality:PropTypes.string.isRequired,
+    caretaker:PropTypes.string.isRequired
+};
+
+
+export default Cat;
